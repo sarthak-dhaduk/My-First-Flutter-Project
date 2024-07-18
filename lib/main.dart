f227a978f4e287ac5278827a411892b2c8de038d
Sarthak <sarthakdhaduk1111@gmail.com>
2024-07-18 15:27:18 +0530

    #2 testing push on git

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/BMS.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
